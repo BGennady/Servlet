4564bf1e28c2a40285c673720be3b6c6ab80db41
Gennady <g.korolew@gmail.com>
2024-11-29 15:43:06 +0400

    Исправление feature/di-java v1

--- src/main/java/ru/netology/JavaConfig.java
+++ src/main/java/ru/netology/JavaConfig.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,10 +8,14 @@ import org.springframework.context.annotation.ComponentScan;
 @Configuration // помечает класс как конфигурацию Spring
 @ComponentScan(basePackages = "ru.netology") // автоматически сканирует пакеты для поиска аннотированных компонентов
 public class JavaConfig {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        var controller = context.getBean(PostController.class);
+    }
 
-    @Bean // метод для создания PostRepository
-    public PostRepository postRepository() {
-        return new PostRepository(); // возвращаем экземпляр PostRepository
+    @Bean // метод для создания PostController
+    public PostController postController(PostService postService) {
+        return new PostController(postService); // возвращаем экземпляр PostController с зависимостью от PostService
     }
 
     @Bean // метод для создания PostService
@@ -18,8 +23,8 @@ public class JavaConfig {
         return new PostService(postRepository); // возвращаем экземпляр PostService с зависимостью от PostRepository
     }
 
-    @Bean // метод для создания PostController
-    public PostController postController(PostService postService) {
-        return new PostController(postService); // возвращаем экземпляр PostController с зависимостью от PostService
+    @Bean // метод для создания PostRepository
+    public PostRepository postRepository() {
+        return new PostRepository(); // возвращаем экземпляр PostRepository
     }
 }
